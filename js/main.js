aca5b22caa7c445b0f8e1b78781ca1bd39ca6035
chinchillapsico <coraline.colous@gmail.com>
2017-05-12 12:18:26 -0300

    cambiossssss

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function mostrarData (){
+	var nombre=getElementById(nombre).value;
+	var apellido=getElementById(apellido).value;
+	var email=getElementById(email).value;
+	var direccion=getElementById(direccion).value;
+	var telefono=getElementById(telefono).value;
+	var mostrar.innnerHTML= 
+
+	if 
+
+
+}
